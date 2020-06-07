5f46d5ece5468c399d12c6f457deb1be7107f626
user <user@users-MacBook-Air.local>
2020-06-06 20:30:54 -0500

    step2 final add

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,22 +7,40 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+      
+//  <div class="tab">javascript</div>
+//  <div class="tab">bootstrap</div>
+//  <div class="tab">technology</div>
+//  <div class="tab">jquery</div>
+//  <div class="tab">node.js</div>
+
+ function getTabs (topic) {
+
+ const thePromise = axios.get('https://lambda-times-backend.herokuapp.com/topics')
+
+thePromise.then(function (response) {
+    console.log('the response from the API, organized for us by axios', response);
+   
 
 
- axios.get('https://lambda-times-backend.herokuapp.com/topics')
+const tabsNow = response.data.message;
 
-.then(function (response) {
-    console.log(response.data);
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.headers);
-    console.log(response.config);
+thePromise.forEach(function(topicTab){
+ const tabForAll = document.createElement('div');
+
+ tabForAll.textContent = javascript;
+ tabsNow .appendChild('div')
+
+ });
+
+ const topics = document.querySelector('.topics');
+ topics.appendChild(tabsNow);
 
 
-    
-  })
+ })
 
   .catch(function (error) {
     console.log(error);
   });
 
+ }
